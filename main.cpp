fd7a5e40dddd5163108c251d0e77acd2630d991f
Leah Restad <leahrestad@Leahs-MBP.fios-router.home>
2022-12-08 08:49:32 -0500

    remove glitching intro

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "Mode.hpp"
 
 #include "MainMenuMode.hpp"
-#include "GP22IntroMode.hpp"
+//#include "GP22IntroMode.hpp"
 
 //For asset loading:
 #include "Load.hpp"
@@ -112,8 +112,8 @@ int main(int argc, char **argv) {
 	call_load_functions();
 
 	//------------ create game mode + make current --------------
-	// Mode::set_current(std::make_shared< PlayMode >());
-	Mode::set_current(std::make_shared< GP22IntroMode >( std::make_shared< MainMenuMode >() ));
+	Mode::set_current(std::make_shared< PlayMode >());
+	//Mode::set_current(std::make_shared< GP22IntroMode >( std::make_shared< MainMenuMode >() ));
 
 	//------------ main loop ------------
 
